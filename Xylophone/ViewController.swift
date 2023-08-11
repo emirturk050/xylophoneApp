0b34b11b004d2e8a79006f1a638678fbf7435b51
Emir Türk <turkemir2152@gmail.com>
2023-08-11 12:54:57 +0300

    first commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,16 +6,42 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    var selectedbutton: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: selectedbutton , withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+    
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        selectedbutton = sender.currentTitle
+        
+        playSound()
+        
+        sender.alpha = 0.5
 
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    
 }
 
